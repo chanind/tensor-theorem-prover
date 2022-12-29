258fdc87f3e19da82c56aa01ac8350d9ce02a23f
David Chanin <chanindav@gmail.com>
2022-12-28 20:18:12 -0500

    chore: try fixing path at top of docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,14 @@
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
 from tensor_theorem_prover import __version__
 
 # -- Project information -----------------------------------------------------
